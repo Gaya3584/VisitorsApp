361cc04fd49e8f87076066d65520555c68205507
Gaya3584 <gayugayus584@gmail.com>
2024-12-18 15:29:27 +0530

    AddVisitor updated

--- app/src/main/java/com/example/visitors/MainActivity.java
+++ app/src/main/java/com/example/visitors/MainActivity.java
@@ -1,19 +1,32 @@
 package com.example.visitors;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    AppCompatButton bt1,bt2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        bt1=(AppCompatButton) findViewById(R.id.b1);
+        bt2=(AppCompatButton) findViewById(R.id.b2);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(), AddActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
